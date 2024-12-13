3dd1323f7fb1c7c25d980a954b49ae371c754d62
Danielabrito2003 <a25591@alunos.ipca.pt>
2024-12-13 18:18:44 +0000

    Início do projeto Apoio360
    - Configuração inicial do projeto no Android Studio.
    - Estrutura base do projeto criada com Kotlin e organização do package `com.example.apoio360`.
    - Adicionado ficheiro `.gitignore`

---
+++ app/src/main/java/com/example/apoio360/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.apoio360
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.apoio360.ui.theme.Apoio360Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Apoio360Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Apoio360Theme {
+        Greeting("Android")
+    }
+}
